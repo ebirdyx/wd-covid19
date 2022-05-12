bfef3a1410ed0436e1a605805c5c3ca42821cad7
Pamela Brahollari <ebirdyxx@gmail.com>
2022-05-12 14:11:44 +0200

    added react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Advice from './pages/Advice';
+import Vaccine from './pages/Vaccine';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Advice />
+
+      <Routes>
+        <Route path="/" element={<Advice />} />
+        <Route path="/vaccine" element={<Vaccine />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Vaccine/index.jsx
@@ -0,0 +1,7 @@
+import './style.css';
+
+function Vaccine() {
+  return <div>vaccine</div>;
+}
+
+export default Vaccine;
