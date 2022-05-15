ad752a9f898c8361229529b4512d1f4c5a8889f4
Pamela Brahollari <ebirdyxx@gmail.com>
2022-05-15 16:27:01 +0200

    added logo

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,7 +3,10 @@ import './index.css';
 function Navbar() {
   return (
     <nav class="navbar">
-      <div class="logo">Covid-19 Health Advice</div>
+      <div className="navbar-logo">
+        <img src="/images/Logo.png" alt="" srcset="" className="logo-nav" />
+        <div class="logo">Covid-19 Health Advice</div>
+      </div>
       <ul class="nav-links">
         <input type="checkbox" id="checkbox_toggle" />
         <label for="checkbox_toggle" class="hamburger">
